7008a24f1c30e695bae93a861551bebea6b5fb89
anjico92broficial <anjico92broficial@gmail.com>
2026-07-06 23:05:51 -0300

    Update arcade-controls.js

--- config/arcade-controls.js
+++ config/arcade-controls.js
@@ -1,1 +1,35 @@
+// =====================================
+// ANJICO ARCADE
+// Configuração Global de Controles
+// =====================================
 
+window.ANJICO_CONTROLS = {
+
+    capcom:{
+
+        lp:"BUTTON_3",               // X
+        mp:"BUTTON_4",               // Y
+        hp:"RIGHT_TOP_SHOULDER",     // RB
+
+        lk:"BUTTON_1",               // A
+        mk:"BUTTON_2",               // B
+        hk:"RIGHT_BOTTOM_SHOULDER",  // RT
+
+        ppp:"LEFT_TOP_SHOULDER",     // LB
+        kkk:"LEFT_BOTTOM_SHOULDER"   // LT
+
+    },
+
+    neogeo:{
+
+        A:"BUTTON_3",                // X
+
+        B:"BUTTON_1",                // A
+
+        C:"BUTTON_4",                // Y
+
+        D:"BUTTON_2"                 // B
+
+    }
+
+};
